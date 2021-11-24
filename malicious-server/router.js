cf817d32286289acf7afb88e41f67363912b757f
BernardoCMoreira <up201604014@fe.up.pt>
2021-11-24 10:47:10 +0100

    Added 'couldn't get session data' to malicious server link

--- malicious-server/router.js
+++ malicious-server/router.js
@@ -8,8 +8,11 @@ router.get("/malicious-url", hack);
 
 function hack(request, response) {
   const cookies = request.cookies;
-  console.log(cookies);
-  return response.status(200).send(`I got your cookies (ˆڡˆ)◞🍪`);
+  if(Object.keys(cookies).length != 0){
+    return response.status(200).send(`I got your cookies (ˆڡˆ)◞🍪`);
+  } else {
+    return response.status(200).send(`I couldn't get your session data :( `);
+  }
 }
 
 export default router;
